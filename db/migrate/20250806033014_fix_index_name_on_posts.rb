80600ebccd1fb9cde701bfb2c174e739e902e62a
EC2 Default User <ec2-user@ip-172-31-40-129.ap-northeast-1.compute.internal>
2025-08-31 06:04:38 +0000

    [fix] migration

--- db/migrate/20250806033014_fix_index_name_on_posts.rb
+++
@@ -1,6 +0,0 @@
-class FixIndexNameOnPosts < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :posts, name: "index_posts_on_genres_id"
-  add_index :posts, :genre_id, name: "index_posts_on_genre_id"
-  end
-end
